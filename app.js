d5f0167512b45480dc7b041892ab10e350ec2aff
Ahmed Raza <aimen69khan@gmail.com>
2026-07-01 20:07:40 +0500

    2 Q Done

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 /*
 Question 1: Variable Declarations and Initialization.
 Create three variables: productName (string with value "Laptop"),
-price (number with value 999.99), and inStock (boolean with value true). 
+price (number with value 999.99) and,
+inStock (boolean with value true). 
 Console.log all three variables in a single statement.
 */
 
@@ -11,4 +12,40 @@ let productName = "Laptop";
 let price = 999.99;
 let inStock = true;
 
-console.log(productName, price, inStock);
+// console.log(productName, price, inStock);
+
+
+
+/*
+Question 2: Mathematical Operations
+Calculate and display:
+
+The remainder when 27 is divided by 4
+The square of 12 (using exponentiation operator **)
+The result of incrementing 8 by 1 using the ++ operator
+The result of decrementing 15 by 2 using the -= operator
+*/
+
+// Answer 2:
+
+let remainder = 27/4;
+// console.log(remainder);
+
+
+const square = 12**2;
+// console.log(square);
+
+
+let increment = 8;
+increment++
+
+// console.log(increment);
+
+
+let decrement = 15;
+--decrement;
+--decrement;
+
+// console.log(decrement);
+
+
